6b558abeb677450ca086495c4454280f5ea6cf45
Sean Widmer <bomberooski@gmail.com>
2024-04-17 20:25:45 -0400

    Update TodoInput.jsx

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,24 +1,27 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 export default function TodoInput(props) {
 
         const {handleAddTodos, todoValue, setTodoValue} = props
-
+        const inputRef = useRef(null)
+        const handleClick = () => {
+            handleAddTodos(todoValue);
+            setTodoValue('');
+            inputRef.current.focus();
+        }
 
 
 
         return (
             <header>
                 <input 
+                    ref = {inputRef}
                     value={todoValue} 
                     onChange={(keystroke) => {
                         setTodoValue(keystroke.target.value)
-                    } } 
+                    } }     
                     placeholder="Enter ToDo Here..." />
-                <button onClick={() => {
-                    handleAddTodos(todoValue)
-                    setTodoValue('')
-                }}>Add</button>
+                <button onClick={handleClick}>Add</button>
             </header>
         )
 }
